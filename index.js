4ad360a1dfefb8a5576ab8fb9ae05a06058baf6e
ariyan salari <ariyan4509@gmail.com>
2022-07-09 14:20:22 -0700

    HW-JS-02

---
+++ index.js
@@ -0,0 +1,15 @@
+const Name=prompt("Enter your name :");
+const AGE=+prompt("Enter your age :");
+let Result=0;
+birthday=2022-AGE;
+if(birthday>=18&&birthday<=20)
+{
+Result=birthday+56;
+}
+else{
+    console.log("ERROR");
+}
+console.log("your birthday is =",AGE);
+console.log("now you are =",birthday);
+alert("+56 years you are "+Result +" years old");
+console.log("+56 years you are =",Result);
